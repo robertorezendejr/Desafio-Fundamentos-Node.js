7e61276d954774afede54d4301c83ccdfeb8acdc
Roberto Rezende Jr <robertorezendejr@hotmail.com>
2021-05-19 19:25:22 -0300

    commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,16 @@
+import { uuid } from 'uuidv4';
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'outcome' | 'income';
 }
 
 class TransactionsRepository {
@@ -14,15 +21,48 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((previous, current) => {
+      if(current.type === 'income') {
+        return previous + current.value;
+      }
+      return previous;
+    },0);
+
+    const outcome = this.transactions.reduce((previous, current) => {
+      if(current.type === 'outcome') {
+        return previous + current.value;
+      }
+      return previous;
+    },0);
+
+    const total = income - outcome;
+
+    return {
+      income,
+      outcome,
+      total,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: RequestDTO): Transaction {
+    const { total } = this.getBalance();
+
+    if(total < value && type === 'outcome') {
+      throw new Error('Sem saldo!😕');
+    }
+    const transaction = {
+      id: uuid(),
+      title,
+      value,
+      type,
+    };
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,13 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'outcome' | 'income';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +15,14 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO ): Transaction {
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value
+    });
+
+    return transaction;
   }
 }
 
